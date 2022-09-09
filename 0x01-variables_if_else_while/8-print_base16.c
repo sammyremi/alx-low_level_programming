84c6c31065fb48a173acb86cd5e6b3409c386bf2
sammyremi <adebayorsamuelas@gmail.com>
2022-09-09 15:55:42 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (letter = 'a'; lett <= 'f'; lett++)
+	for (lett = 'a'; lett <= 'f'; lett++)
 		putchar(lett);
 
 	putchar('\n');
