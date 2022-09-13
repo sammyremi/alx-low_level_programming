cdfea9801dab417912efe0542dee9dff2e902bca
sammyremi <adebayorsamuelas@gmail.com>
2022-09-13 10:49:39 -0700

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -Entry point
+ * print_alphabet - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -15,5 +15,4 @@ void print_alphabet(void)
 		alpha++;
 	}
 	_putchar('\n');
-	return (void);
 }
