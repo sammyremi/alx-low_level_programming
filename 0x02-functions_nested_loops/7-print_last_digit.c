23126a9f1da45200f4fc0c6ecb0db0239c27b4ad
sammyremi <adebayorsamuelas@gmail.com>
2022-09-13 16:25:56 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ *@r:number to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int r)
+{
+	r = (r % 10);
+	return (r);
+}
