b13589dba5a85c6154498973b9e518e4fc2b38e3
sammyremi <adebayorsamuelas@gmail.com>
2022-09-13 13:01:17 -0700

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+	_putchar('\n');
+}
