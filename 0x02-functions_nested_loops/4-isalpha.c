61f7c6c2db1e5bc429a05b284e45d140adf7afa1
sammyremi <adebayorsamuelas@gmail.com>
2022-09-13 15:05:14 -0700

    done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  *
  * Return: Always 0 (Success)
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
