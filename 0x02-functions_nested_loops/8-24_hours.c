4e82ffcf96da72aba04ea13312329978d26f49e1
sammyremi <adebayorsamuelas@gmail.com>
2022-09-13 18:32:37 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,5 +9,19 @@
  */
 void jack_bauer(void)
 {
-	return (0);
+	int x = 0;
+	int y = 0;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (y = 0; y <= 59; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
