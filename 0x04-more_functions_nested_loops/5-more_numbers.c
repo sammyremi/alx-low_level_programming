277ca5f14f388fa9355cd0055041fd73fd07c6e7
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 14:10:14 -0700

    print more numbers from 0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - from 0 -14
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0';)
+			}
+			else
+			{
+				_putchar(y + '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
