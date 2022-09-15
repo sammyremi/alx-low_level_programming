590ee5ac5709040fe2a3834633054126e2b52a6a
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 14:17:36 -0700

    print number from 0 - 14 in 10 lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,8 @@ void more_numbers(void)
 		for (y = 0; y <= 14; y++)
 		{
 			if (y >= 10)
-			{
-				_putchar((y / 10) + '0');
-				_putchar((y % 10) + '0');
-			}
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 	_putchar('\n');
 	}
