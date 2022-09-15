5dd7cabc59acfc4eaf4846f82bb3f45b5f392b3e
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 09:46:48 -0700

    print numbers from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * return:number to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	for (int c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+void print_numbers(void);
+_putchar(int c);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
