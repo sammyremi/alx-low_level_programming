780212291999254ae6ef37b2a5efe5a63b88b7ef
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 12:19:09 -0700

    print number 0 - 9 with putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - _putchar 0 - 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}
+}
