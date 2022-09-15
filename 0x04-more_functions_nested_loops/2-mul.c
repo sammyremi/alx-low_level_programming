798e49481c14563216621fdab49775d06baf8a4a
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 09:30:31 -0700

    multiply 2 number return a and b

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * 2-mul - Entry point
+ * mul - Entry point
  *
- * @c:number to be checked
+ * @a:number to be returned
+ *
+ * @b: nuber to be returned
  *
  * Return: Always 0 (Success)
  */
 int mul(int a, int b)
 {
+	int a;
+	int b;
+
 	return (a * b);
 }
