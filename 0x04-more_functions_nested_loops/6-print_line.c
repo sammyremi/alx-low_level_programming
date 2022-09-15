8a6bd77a0fe2f205c58e5183219321b753c88f3c
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 15:56:51 -0700

    print line the number of n

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - from n equal _
+ * @n: integer to count for times to print
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x <= n; x++)
+	{
+		if (x <= 0)
+		_putchar('\n');
+
+	_putchar('_');
+	}
+	_putchar('\n');
+}
