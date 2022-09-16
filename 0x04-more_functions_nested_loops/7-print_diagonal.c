ca56bf6eff4580f8f848ad97b0c87250273705b6
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 18:44:33 -0700

    print square with # with newline

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - from n equal _
+ * @n: integer to count for times to print
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
