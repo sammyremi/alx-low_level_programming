2eaae3f36085af8ac7fea5df65b8910e1050cd39
sammyremi <adebayorsamuelas@gmail.com>
2022-09-15 19:15:12 -0700

    printing diagonal with newline and backslash

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,32 @@
 #include "main.h"
 
 /**
- * print_line - from n equal _
- * @n: integer to count for times to print
+ * print_diagonal - from n equal _
+ * @n: number of times to print chracter to screen
  * Return: Always 0
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
-	int x;
-
-	for (x = 0; x < n; x++)
+	if (n == 0 || n < 0)
 	{
-		if (n <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+
+		for (x = 0; x < n; x++)
 		{
+			int y;
+
+			for (y = 0; y <= x; y++)
+			{
+				if (y == x)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
-		else
-		{
-			_putchar('_');
-		}
 	}
-	_putchar('\n');
 }
