6c66e27a86af0e14eaa95cf0c764b90634ea86c7
sammyremi <adebayorsamuelas@gmail.com>
2022-09-19 08:51:59 -0700

    swap *a for *b pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - *a *b passed in pointer
+ * @a: pointer variable
+ * @b: pointer variable
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+}
