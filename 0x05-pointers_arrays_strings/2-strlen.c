ffe1881feb838ec06fb1e2ef36c454affc56d7ac
sammyremi <adebayorsamuelas@gmail.com>
2022-09-19 10:02:15 -0700

    check len of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - check len of variable
+ * @s: var pointer to check
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = _strlen(*s);
+
+	return (len);
+}
