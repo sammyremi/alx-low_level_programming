624b778b5bbd8005ff6d51579992fea6fa8049e4
sammyremi <adebayorsamuelas@gmail.com>
2022-09-21 18:26:45 -0700

    removing returned pointer and returning dest

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,27 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *point;
 	int i = 0;
 	int j = 0;
-	char fstring[];
-
-	point = &fstring;
 
 	while (dest[i] != '\0')
 	{
-		fstring[i] = dest[i];
 		i++;
-		j++;
 	}
 
-	i = 0;
 	while (src[i] != '\0')
 	{
-		fstring[j] = src[i];
+		dest[j] = src[i];
 		i++;
 		j++;
 	}
-	fstring[j] = '\0';
-	return (point);
+	dest[j] = '\0';
+	return (dest);
 }
