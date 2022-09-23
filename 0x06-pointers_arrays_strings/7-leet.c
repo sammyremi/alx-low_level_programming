7a94ad5a2a4ec4de2de710f8692e0d209e403b72
sammyremi <adebayorsamuelas@gmail.com>
2022-09-22 18:59:00 -0700

    return encode 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encodes string to 1337
+ * @s: string to check
+ * Return: Always 0
+ */
+char *leet(char *s)
+{
+	int a;
+	int b;
+
+	char x[] = "aAeEoOtTL";
+	char y[] = "4433007711";
+
+	for (a = 0; *(s + b); a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (x[b] == s[a])
+				s[a] = y[b];
+		}
+	}
+	return (s);
+}
