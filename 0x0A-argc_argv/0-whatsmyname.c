a5465b10bd56402eb7a47c0796ecbab603c455b8
sammyremi <adebayorsamuelas@gmail.com>
2022-09-30 11:40:05 -0700

    printing name of program with newline

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - name of my function
+ * @argc: count
+ * @argv: vector
+ * Return: return 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
