e8528186073bd058eff72e0f4449376e553ee184
sammyremi <adebayorsamuelas@gmail.com>
2022-10-06 07:43:54 -0700

    allocate memory to unsigned int

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory to int
+ * @b: unsigned int to assign mem
+ * Return: return pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+		return (ptr);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,5 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - return pointer new concat mem string
+ * @s1: 
