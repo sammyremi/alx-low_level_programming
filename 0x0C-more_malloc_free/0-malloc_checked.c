21a30f76e152d6f6759eeb9828fc8dcac9983d9b
sammyremi <adebayorsamuelas@gmail.com>
2022-10-06 10:42:02 -0700

    allocate memory to unsigned int

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limit.h>
 #include <stdio.h>
 #include <stdlib.h>
 /**
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,4 +2,11 @@
 #include <stdlib.h>
 /**
  * string_nconcat - return pointer new concat mem string
- * @s1: 
+ * @s1: first string
+ * @s2: nbytes
+ * Return: concat pointer string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
