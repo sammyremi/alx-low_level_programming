7bd4b1bcfd95554252623c847ecdbdc6b7ad20d2
sammyremi <adebayorsamuelas@gmail.com>
2022-10-06 12:11:57 -0700

    string concat and return concat n byte of pointer

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,10 +3,40 @@
 /**
  * string_nconcat - return pointer new concat mem string
  * @s1: first string
- * @s2: nbytes
+ * @s2: secomd string
+ * @n: n bytes
  * Return: concat pointer string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int x;
+	unsigned int y;
+	unsigned int i;
+	unsigned int j;
+	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (x = 0; s1[x] != '\0'; x++)
+		;
+	for (y = 0; s2[y] != '\0'; y++)
+		;
+	ptr = malloc(x + n + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
 }
