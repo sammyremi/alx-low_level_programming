2ea72a418cdebe79975ceabab2b91ef998858291
sammyremi <adebayorsamuelas@gmail.com>
2022-10-06 18:11:59 -0700

    create array range of int return pointer

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - create array range
+ * @min: minmum int
+ * @max: maximum num int
+ * Return: return array range
+ */
+int *array_range(int min, int max)
+{
+	int x;
+	int *ptr;
+	int y;
+
+	if (min > max)
+		return (NULL);
+
+	y = min + max;
+
+	ptr = malloc(y * sizeof(int) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (x = 0; min <= max; x++)
+	{
+		ptr[x] = min;
+		min++;
+	}
+	
+	return (ptr);
+}
