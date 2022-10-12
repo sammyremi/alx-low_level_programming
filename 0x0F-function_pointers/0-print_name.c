21271101f8bc6790f745f401de1b2fdc44638e69
sammyremi <adebayorsamuelas@gmail.com>
2022-10-12 13:41:36 -0700

    execute function with array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,10 @@
 /**
  * print_name - print name of pass to function
  * @name: name to print
+ * @f: fuction pointer
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,17 @@
 #include "function_pointers.h"
 /**
- * array_iterator - 
+ * array_iterator - execute function of element array
  * @size: size of array
+ * @array: array to be passed
  * @action: pointer to the function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int x;
 
+
+	for (x = 0; x < size; x++)
+	{
+		action(array[x]);
+	}
 }
